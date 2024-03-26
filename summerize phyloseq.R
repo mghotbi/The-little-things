991548b1cb210b92f8384e56f85b79910c9d179b
Mitra <mitra.ghotbi@gmail.com>
2024-03-26 10:04:24 -0500

    Update summerize phyloseq.R

--- summerize phyloseq.R
+++ summerize phyloseq.R
@@ -1,3 +1,4 @@
+#summary based on ASV_ID
 summary_phyloseq <- function(physeq) {
   otu_table <- otu_table(physeq)
   summary_stats <- data.frame(
@@ -10,3 +11,16 @@ summary_phyloseq <- function(physeq) {
   return(summary_stats)
 }
 
+
+#summary based on sample_id
+summ_phyloseq <- function(physeq) {
+  otu_table <- otu_table(physeq)
+  summary_stats <- data.frame(
+    Sample_ID = colnames(otu_table),
+    Mean = apply(otu_table, 2, mean),
+    Median = apply(otu_table, 2, median),
+    SD = apply(otu_table, 2, sd),
+    SE = apply(otu_table, 2, function(x) sd(x) / sqrt(length(x)))
+  )
+    return(summary_stats)
+}
