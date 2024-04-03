b54cc74cdb45d83aa5feffc0bb5d4c70f71e7da0
Mitra <mitra.ghotbi@gmail.com>
2024-04-03 11:58:26 -0500

    Add files via upload

---
+++ Summ_ASV_ID.R
@@ -0,0 +1,12 @@
+#summary based on ASV_ID 
+summary_phyloseq <- function(physeq) {
+  otu_table <- otu_table(physeq)
+  summary_stats <- data.frame(
+    ASV_ID = rownames(otu_table),
+    Mean = apply(otu_table, 1, mean),
+    Median = apply(otu_table, 1, median),
+    SD = apply(otu_table, 1, sd),
+    SE = apply(otu_table, 1, function(x) sd(x) / sqrt(length(x)))
+  )
+  return(summary_stats)
+}
