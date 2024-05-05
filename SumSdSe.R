da96551bae665bb2ce2ef06e31821679958af960
Mitra <mitra.ghotbi@gmail.com>
2024-05-05 11:09:40 -0500

    Update SumSdSe.R

--- SumSdSe.R
+++ SumSdSe.R
@@ -1,11 +1,14 @@
 library(dplyr)
 
-calculate_summary_stats <- function(data) {
+calculate_summary_stats <- function(data, grouping_var) {
   summary_stats <- data %>%
-    group_by() %>%
+    group_by({{ grouping_var }}) %>%
     select_if(is.numeric) %>%
     summarise_all(list(mean = ~mean(.), sd = ~sd(.), se = ~sd(./sqrt(.))))
   
   return(summary_stats)
 }
+
+#Example
+#summary_stats <- calculate_summary_stats(data, factor)
 
